4e94ad3aae2c73b9b392435f858e39b2c4ddb2f9
Jeremiah Franco <jeremiahfranco3@gmail.com>
2026-04-11 20:00:19 -0700

    Made transition styles registry -> paq vice versa

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import image from '/faq_image.webp';
 
 export function Faq() {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
@@ -61,7 +60,7 @@ export function Faq() {
           ))}
         </div>
       </div>
-      <img src={image} alt="image" />
+      {/* <img src={image} alt="image" /> */}
     </section>
   );
 }
--- src/components/Registry.tsx
+++ src/components/Registry.tsx
@@ -1,21 +1,31 @@
-import amazon from '/amazon-logo.png'
+import amazon from '/amazon_logo.webp'
 import venmo from '/venmo.jpg';
+import image from '/faq_image.webp'
 
 export function Registry() {
   return (
     <div id="registry">
-      <h1 className="bellota-regular">Registry</h1>
-      <p>We’re so grateful to have you in our lives and to celebrate this special moment with us. 
-        If you’d like to contribute to our next chapter, we’ve created this registry with a few 
-        things we’d love. Your presence is truly the greatest gift.
-      </p>
-      <div id="registryLinks">
-        <a href="https://www.amazon.com/wedding/share/jeremiahandjanelle" target="blank"><img  id="amazonLogo" src={amazon} alt="amazon-logo" /></a>
-        <div id="venmo">
-          <a href="https://venmo.com/u/jerfranco" target="blank"><img src={venmo} alt="venmo qr code" /></a>
+      <div id="registryInfo">
+        <h1 className="bellota-regular">Registry</h1>
+        <p>We’re so grateful to have you in our lives and to celebrate this special moment with us. 
+          If you’d like to contribute to our next chapter, we’ve created this registry with a few 
+          things we’d love. Your presence is truly the greatest gift.
+        </p>
+        <div id="registryLinks">
+          <div id="amazonLogo">
+            <a href="https://www.amazon.com/wedding/share/jeremiahandjanelle" target="blank"><img src={amazon} alt="amazon-logo" /></a>
+          </div>
+          <div id="venmo">
+            <a href="https://venmo.com/u/jerfranco" target="blank"><img src={venmo} alt="venmo qr code" /></a>
+          </div>
+          
         </div>
-        
       </div>
+      <div id="rightImg">
+        <img src={image} alt="Janelle and Jeremiah" />
+      </div>
+      
+      
     </div>
   )
 }
