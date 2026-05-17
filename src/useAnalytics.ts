bfe00d5b875e4fdcb043f76045895a758abe34ec
Jeremiah Franco <jeremiahfranco3@gmail.com>
2026-05-16 23:52:15 -0700

    Testing google analytics

--- src/useAnalytics.ts
+++ src/useAnalytics.ts
@@ -1,15 +1,13 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-
 export function useAnalytics(): void {
   const location = useLocation();
 
   useEffect(() => {
-    if (typeof window !== "undefined" && (window as any).gtag) {
-      (window as any).gtag("event", "page_view", {
+    if ((window as any).gtag) {
+      (window as any).gtag("config", "G-RWNSXTEJN5", {
         page_path: location.pathname,
-        page_title: document.title,
         page_location: window.location.href,
       });
     }
