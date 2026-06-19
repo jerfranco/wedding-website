b7b4a076fb3958535c1e060a391c432b4df93476
Jeremiah Franco <jeremiahfranco3@gmail.com>
2026-06-19 01:19:37 -0700

    Added countdown timer

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,34 +1,93 @@
 import { useLocation } from "react-router-dom"
-import image from '/home_image_mini.webp'
-import imageRight from '/flower_right.webp'
-import imageLeft from '/flower_left.webp'
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import image from "/home_image_mini.webp"
+import imageRight from "/flower_right.webp"
+import imageLeft from "/flower_left.webp"
 
 export function Home() {
-  useEffect(() => { document.title = "Home | Janelle & Jeremiah"; }, []);
+  useEffect(() => {
+    document.title = "Home | Janelle & Jeremiah"
+  }, [])
+
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  })
+
+  useEffect(() => {
+    const weddingDate = new Date("2026-08-08T00:00:00")
+
+    const timer = setInterval(() => {
+      const now = new Date()
+      const difference = weddingDate.getTime() - now.getTime()
+
+      if (difference <= 0) {
+        clearInterval(timer)
+        return
+      }
+
+      setTimeLeft({
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor(
+          (difference % (1000 * 60 * 60 * 24)) /
+            (1000 * 60 * 60)
+        ),
+        minutes: Math.floor(
+          (difference % (1000 * 60 * 60)) /
+            (1000 * 60)
+        ),
+        seconds: Math.floor(
+          (difference % (1000 * 60)) /
+            1000
+        ),
+      })
+    }, 1000)
+
+    return () => clearInterval(timer)
+  }, [])
 
   const location = useLocation()
-
   const submitted = location.state?.submitted
 
   return (
     <section id="homeWrapper">
       <section id="home">
         <div id="homeImg">
-          {/* <img src={image} /> */}
           <h2 className="bellota-regular">8 / 8 / 2026</h2>
+
+          <div id="countdown" className="barlow-regular">
+            <div>
+              <span>{timeLeft.days}</span>
+            </div>
+            <span className="colon">:</span>
+            <div>
+              <span>{timeLeft.hours}</span>
+            </div>
+            <span className="colon">:</span>
+            <div>
+              <span>{timeLeft.minutes}</span>
+            </div>
+            <span className="colon">:</span>
+            <div>
+              <span>{timeLeft.seconds}</span>
+            </div>
+          </div>
+
           <p className="barlow-regular">Sealing - Oakland, CA</p>
           <p className="barlow-regular">Reception - Brentwood, CA</p>
         </div>
 
         <div id="homeContent">
-          {/* <h2>
-            August 8, 2026
-          </h2> */}
           <h1 className="great-vibes-regular">
-            <span id="janelle">Janelle</span><span id="and"><br /> & <br /></span><span id="jeremiah">Jeremiah</span>
+            <span id="janelle">Janelle</span>
+            <span id="and">
+              <br />
+              & <br />
+            </span>
+            <span id="jeremiah">Jeremiah</span>
           </h1>
-          
 
           {submitted && (
             <p style={{ color: "green", fontWeight: "bold" }}>
@@ -37,19 +96,27 @@ export function Home() {
           )}
         </div>
       </section>
+
       <section id="inviteSection">
         <p className="barlow-regular">
-        You are invited to the wedding of Janelle Stewart & Jeremiah Franco<br />
-        <strong>Please RSVP by July 1st</strong> and get ready for some fun!
+          You are invited to the wedding of Janelle Stewart & Jeremiah Franco
+          <br />
+          <strong>Please RSVP by July 1st</strong> and get ready for some fun!
         </p>
-        {/* <p>Please RSVP <a href="/rsvp">here</a> by July 1st</p> */}
-        <p id="salamat" className="great-vibes-regular">Maraming salamat po!</p>
+
+        <p id="salamat" className="great-vibes-regular">
+          Maraming salamat po!
+        </p>
+
         <div id="miniHome">
-          <img src={imageLeft} alt="Flower left side"></img>
-          <img id="homeMiddlePic" src={image} alt="Holding hands"></img>
-          <img src={imageRight} alt="Flower right side"></img>
+          <img src={imageLeft} alt="Flower left side" />
+          <img
+            id="homeMiddlePic"
+            src={image}
+            alt="Holding hands"
+          />
+          <img src={imageRight} alt="Flower right side" />
         </div>
-        
       </section>
     </section>
   )
